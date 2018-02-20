cf10c38f3ab7f0e4f24b528987deb87b7fb0a3ee
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2018-02-20 01:19:26 +0100

    Variable declarations

--- designs.js
+++ designs.js
@@ -1,6 +1,14 @@
 // Select color input
 // Select size input
 
+const form   = document.querySelector("#sizePicker");
+const height = form.querySelector("#inputHeight");
+const weight  = form.querySelector("#inputWeight");
+const submit = form.querySelector(".btn");
+
+const color_picker  = document.querySelector("#colorPicker");
+const canvas = document.querySelector("#pixelCanvas");
+
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
